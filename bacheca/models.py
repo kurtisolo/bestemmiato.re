4f60125a24bdfa25a7a53dc54471e7ef8d91f7e3
Kurtisolo <kurtisolo@192.168.1.2>
2023-07-18 14:55:56 +0200

    modifica bacheca.models
    dal modello BestemmiaBacheca rimosso likes, migrazioni effettuate

--- bacheca/models.py
+++ bacheca/models.py
@@ -5,7 +5,6 @@ from datetime import datetime
 class BestemmiaBacheca(models.Model):
     bestemmia_bacheca = models.CharField(max_length=500)
     data_bestemmia = models.DateTimeField(default=datetime.now)
-    likes_bestemmia = models.IntegerField(default=0)
     audio_file_path = models.CharField(max_length=255, blank=True)
     autore = models.CharField(max_length=100, default='Anonimo')
     def __str__(self):
